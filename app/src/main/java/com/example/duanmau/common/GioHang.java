90c4c4cd729c25ea03033c103151a8a847ecaa93
VietHoai1 <hoaivvph39292@fpt.edu.vn>
2025-12-04 00:48:30 +0700

    hoai commit code du an mau

---
+++ app/src/main/java/com/example/duanmau/common/GioHang.java
@@ -0,0 +1,29 @@
+package com.example.duanmau.common;
+
+import com.example.duanmau.model.SanPham;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GioHang {
+    private List<GioHangItem> danhSachGioHang;
+
+    public GioHang() {
+        danhSachGioHang = new ArrayList<>();
+    }
+
+    public void addSanPham(SanPham sp) {
+        for (GioHangItem item : danhSachGioHang) {
+            if (item.getSanPham().getMaSanPham().equals(sp.getMaSanPham())) {
+                item.setSoLuong(item.getSoLuong() + 1);
+                return;
+            }
+        }
+        GioHangItem gioHangItem = new GioHangItem(sp, 1);
+        danhSachGioHang.add(gioHangItem);
+    }
+
+    public List<GioHangItem> getDanhSachGioHang() {
+        return danhSachGioHang;
+    }
+}
